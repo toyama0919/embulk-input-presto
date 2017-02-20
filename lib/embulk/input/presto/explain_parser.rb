3d29147c231f03caf062b56d77c5972eb7713d60
y-matsuwitter <info@yuki-matsumoto.com>
2017-02-20 15:29:30 +0900

    FIX an incorrect regular expression.

--- lib/embulk/input/presto/explain_parser.rb
+++ lib/embulk/input/presto/explain_parser.rb
@@ -6,7 +6,7 @@ module Embulk
           explain_text = explain_result.flatten.last.lines.first
           column_name_raw, column_type_raw = explain_text.split(' => ')
           names = column_name_raw.split('[').last.split(']').first.split(',').map{ |name| name.strip }
-          types = column_type_raw.split('[').last.split(']').first.gsub(/\(.+?, .+?\)/, "").split(',').map{ |info| info.split(':').last }
+          types = column_type_raw.split('[').last.split(']').first.gsub(/\(.+?\)/, "").split(',').map{ |info| info.split(':').last }
           names.zip(types)
         end
       end
